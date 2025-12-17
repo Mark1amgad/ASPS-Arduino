777a759720e9e1ee66bce021dfbac8dbaa6902e2
Mark1amgad <markbotros52@gmail.com>
2025-12-17 18:36:44 +0200

    Add parking safety and environment Arduino code
    Initial commit for the parking safety controller.

---
+++ src/parking_safety_controller.ino
@@ -0,0 +1,11 @@
+/*
+ * Automated Smart Parking System (ASPS)
+ * File: parking_safety_controller.ino
+ * Board: Arduino Uno
+ * Responsibility:
+ * - Slot IR sensors
+ * - Ultrasonic collision warning
+ * - Flame detection
+ * - LCD display
+ * - Buzzer alerts
+ */ 
