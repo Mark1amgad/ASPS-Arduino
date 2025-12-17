d3b142164563459644e33a0e1b788ead1803c69e
Mark1amgad <markbotros52@gmail.com>
2025-12-17 18:34:34 +0200

    Add gate and access control Arduino code
    Initial implementation of gate access controller for ASPS.

---
+++ src/gate_access_controller.ino
@@ -0,0 +1,10 @@
+/*
+ * Automated Smart Parking System (ASPS)
+ * File: gate_access_controller.ino
+ * Board: Arduino Uno
+ * Responsibility:
+ * - Entry & exit IR sensors
+ * - Gate servo motors
+ * - RFID VIP access
+ * - System-level decisions
+ */
